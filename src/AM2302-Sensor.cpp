7a464ca43620aa500fbf9f6013726760f30e8422
Hasenradball <mail@frankhaefele.de>
2023-11-21 19:01:24 +0100

    started implementing the library

--- src/AM2302-Sensor.cpp
+++ src/AM2302-Sensor.cpp
@@ -9,3 +9,120 @@
 */
 
 #include <AM2302-Sensor.h>
+
+int8_t AM2302::AM2302_Sensor::read() {
+    // *****************************
+    //  === send start sequence ===
+    // ****************************
+    // start from HIGH ==> switch to LOW for min. of 1 ms
+    // Set pin to Output
+    pinMode(_pin, OUTPUT);
+    // set Pin to LOW 
+    digitalWrite(_pin, LOW);
+    // wait min. 1,0 ms
+    delay_us(1200U);
+    // Set port to HIGH ==> Input with PullUp
+    pinMode(_pin, INPUT_PULLUP);
+    // delay_us(30.0); not needed
+
+    // *****************************
+    //  === wait for Acknoledge ===
+    // ****************************
+    // Acknowlege Sequence 80us LOW 80 us HIGH
+    // wait for LOW (80 µs)
+    await_state(0);
+    // wait for HIGH (80 µs)
+    await_state(1);
+
+    // *****************************
+    //  ==== Read Sensor Data ====
+    // *****************************
+    // ==> START of time critical code <==
+    int8_t data_buffer[40U];
+    // read 40 bits from sensor:
+    // ==> HIGH state is 70 µs
+    // ==> LOW state is 28 µs
+    //for (uint8_t i = 0; i < 40U; ++i) {
+    if (read_sensor_data(data_buffer, 40U) == ERROR_TIMEOUT) {
+        return ERROR_TIMEOUT;
+    }
+    // ==> END of time critical code <==
+    uint8_t count{0};
+    int8_t val;
+    for (uint8_t i = 0; i < 5U; ++i) {
+        for (uint8_t j = 0; j < 8U; ++j) {
+            _data[i] <<= 1;
+            val = data_buffer[count++];
+            _data[i] |= val;
+        }
+    }
+    // check checksum
+    _checksum_ok = (_data[4] == ( (_data[0] + _data[1] + _data[2] + _data[3]) & 0xFF) );
+
+    if (_checksum_ok) {
+        _hum  = (_data[0] << 8) | _data[1];
+        _temp = (_data[2] << 8) | _data[3];
+        return READ_OK;	
+    }
+    else {
+        return ERROR_CHECKSUM;
+    }
+}
+
+/**
+ * @brief wait for a specific pin state
+ * 
+ * @param state state to wait for
+ * @return int8_t status
+ */
+int8_t AM2302::AM2302_Sensor::await_state(uint8_t state) {
+    uint8_t wait_counter{1}, state_counter{1};
+    // count wait for state time
+    while ( (digitalRead(_pin) != state) ) {
+        ++wait_counter;
+        _delay_us(1.0);
+        if (wait_counter >= READ_TIMEOUT) {
+            return ERROR_TIMEOUT;
+        }
+    }
+    // count state time
+    while ( (digitalRead(_pin) == state) ) {
+        ++state_counter;
+        _delay_us(1.0);
+        if (state_counter >= READ_TIMEOUT) {
+            return ERROR_TIMEOUT;
+        }
+    }
+    return (state_counter > wait_counter);
+}
+
+/**
+ * @brief read sensor data
+ * 
+ * @param buffer data buffer of 40 bit
+ * @param size of buffer => 40
+ * @return int8_t 
+ */
+int8_t AM2302::AM2302_Sensor::read_sensor_data(int8_t *buffer, uint8_t size) {
+    for (uint8_t i = 0; i < size; ++i) {
+        uint8_t wait_counter{0}, state_counter{0};
+        // count wait for state time
+        while ( (!digitalRead()) ) {
+            ++wait_counter;
+            _delay_us(1.0);
+            if (wait_counter >= READ_TIMEOUT) {
+                return ERROR_TIMEOUT;
+            }
+        }
+        // count state time
+        while ( (digitalRead()) ) {
+            ++state_counter;
+            _delay_us(1.0);
+            if (state_counter >= READ_TIMEOUT) {
+                return ERROR_TIMEOUT;
+            }
+        }
+        buffer[i] = (state_counter > wait_counter);
+    }
+    return READ_OK;
+}
--- src/AM2302-Sensor.h
+++ src/AM2302-Sensor.h
@@ -1,4 +1,4 @@
-/*
+/*__AM2302_SENSOR_H__
 *	AM2302-Sensor.h
 *
 *	Author: Frank Häfele
@@ -11,28 +11,34 @@
 #ifndef __AM2302_SENSOR_H__
 #define __AM2302_SENSOR_H__
 
+#include <Arduino.h>
+
 namespace AM2302 {
 
-	//constexpr int8_t DHT_READ_OK {0};
-	//constexpr int8_t DHT_ERROR_CHECKSUM	{-1};
-	//constexpr int8_t DHT_ERROR_TIMEOUT	{-2};
+    constexpr int8_t READ_OK 		{0};
+    constexpr int8_t ERROR_CHECKSUM	{-1};
+    constexpr int8_t ERROR_TIMEOUT	{-2};
 
-	// define timeout in 100 µs
-	//constexpr uint8_t DHT_TIMEOUT		{100U};
+    // define timeout in 100 µs
+    constexpr uint8_t READ_TIMEOUT	{100U};
 
-	class AM2302 {
-		
-		public:
-			void setup();
-			read();
-			get_Temperature();
-			get_Hunidity();
-		
-		private:
-			int8_t _pin;
-			uint16_t _hum {0};
-			int16_t _temp {0};
-			uint8_t _data[5];
-	};
+    class AM2302_Sensor {
+        
+        public:
+            void setup();
+            int8_t read();
+            float get_Temperature() {return _temp * 0.1F;}
+            float get_Hunidity() {return _hum * 0.1F;}
+
+        
+        private:
+            uint8_t _data[5];
+            uint16_t _hum {0};
+            int16_t _temp {0};
+            uint8_t _pin;
+            bool _checksum_ok {false};
+
+            int8_t read_sensor_data(int8_t *buffer, uint8_t size);
+    };
 }
 #endif
